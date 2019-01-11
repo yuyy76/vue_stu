fabbabe663843fd4a286fa0169d18ea0eedf4d09
yuyy76 <dl_yyy@sina.com>
2019-01-11 15:21:32 +0800

    vscode 1.00

--- vuedemo1/src/App.vue
+++ vuedemo1/src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    {{ hello }}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data () {
+    return {
+      hello: 'world'
+    }
+  }
 }
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  text-align: center;
 }
 </style>
